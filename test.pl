b731f50c942c75d1dc01dbe1fafa40b961cd48b5
Shoichi Kaji <skaji@cpan.org>
2014-10-06 00:48:25 +0900

    tags

--- test.pl
+++ test.pl
@@ -7,7 +7,7 @@ use POSIX qw(strftime);
 
 open my $fh, ">", "release.txt" or die;
 
-print {$fh} strftime("%F %T", localtime), "\n";
+print {$fh} strftime("%F %T %z", localtime), "\n";
 print {$fh} "ARGV$_: $ARGV[$_]\n" for 0..$#ARGV;
 
 
