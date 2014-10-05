015deae98de816cd545c93d62552bf7cd797a140
Shoichi Kaji <skaji@cpan.org>
2014-10-06 00:32:27 +0900

    use env

--- test.pl
+++ test.pl
@@ -4,13 +4,4 @@ use warnings;
 use utf8;
 use 5.010;
 
-sub run {
-    my @cmd = @_;
-    say "==> @cmd";
-    chomp( my @out = `@cmd 2>&1` );
-    say "    $_" for @out;
-}
-
-for my $num (31..37) {
-    print "\e[1;${num}m$num ansii color test\e[m\n";
-}
+print "ARGV$_: $ARGV[$_]\n" for 0..$#ARGV;
